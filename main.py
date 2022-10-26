67329a5bfae9ed9a550d5a6c746dbcc13a72944d
TanoR <109026804+Athenator@users.noreply.github.com>
2022-10-27 00:09:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
 
-updater = Updater('5762422332:AAHeP30DDgXSFJlj6O_jHYOuTqFXvn9sKOs')
+updater = Updater('TOKEN')
 updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
 updater.dispatcher.add_handler(CommandHandler('help', help_command))
